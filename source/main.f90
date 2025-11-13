3d29948b7776f30e022edce622602ef3083841a8
Lila Zapp <lilajulika.zapp@gmail.com>
2025-11-13 15:53:13 +0100

    cleanup

--- source/main.f90
+++
@@ -1,9 +0,0 @@
-
-program MD 
-
-CALL parser(xyzfile,topofile,n_atoms,n_bonds,n_angles,n_torsions,positions,atomtypes,bond_const,bond_dist,angle_const,tors_const,&
-    n_value,tors_phase,lj_eps,lj_sigma,charges)
-
-
-
-end program
