33edf610102fa3d4a52b2da5e507ec69b526e347
Lila Zapp <lilajulika.zapp@gmail.com>
2025-11-13 15:51:44 +0100

    cleanup

---
+++ src/main.f90
@@ -0,0 +1,9 @@
+
+program MD 
+
+CALL parser(xyzfile,topofile,n_atoms,n_bonds,n_angles,n_torsions,positions,atomtypes,bond_const,bond_dist,angle_const,tors_const,&
+    n_value,tors_phase,lj_eps,lj_sigma,charges)
+
+
+
+end program
---
+++ src/parser.f90
@@ -0,0 +1,12 @@
+subroutine parser(xyzfile,topofile,n_atoms,n_bonds,n_angles,n_torsions,positions,atomtypes,bond_const,bond_dist,angle_const,tors_const,&
+    n_value,tors_phase,lj_eps,lj_sigma,charges)
+
+implicit none
+
+character, intent(in) :: xyzfile, topofile 
+integer :: n_atoms,n_bonds,n_angles,n_torsions
+real, allocatable, intent(out) :: positions(:,:)
+character, allocatable, intent(out) :: atomtypes(:)
+real, allocatable, intent(out) :: positions(:,:),bond_const(:),bond_dist(:),angle_const(:),tors_const(:),tors_phase(:),&
+                                  n_value(:), lj_eps(:),lj_sigma(:), charges(:)
+end subroutine
