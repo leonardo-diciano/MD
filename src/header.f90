526e3ffead43039a94206fc4576d9a3c0a263ca0
Lila Zapp <lilajulika.zapp@gmail.com>
2025-12-05 18:27:27 +0100

    added some phrases

--- src/header.f90
+++ src/header.f90
@@ -38,17 +38,32 @@ subroutine final_phrase()
 
     phrases = [character(len=256) :: &
     "I'll break it down just to feel okay, I'll tear it down just to feel the pain."&
-    & //achar(10)// "Sorrower, Orbit Culture", &
+    & //achar(10)// "Sorrower, ~ Orbit Culture", &
+    
     "I'm scared to get close, and I hate being alone." & 
     &//achar(10)// "Can You Feel My Heart?, Bring Me The Horizon", &
     "Everybody showed up for the execution but nobody would show their face." & 
     &//achar(10)// "One Assasination Under God, Marilyn Manson", &
+    
     "The only dream that matters is the one you wake up from."& 
     &//achar(10)// "Faithless by Default, Dark Tranquillity", &
+    
     "Fear is the weakness in all of us, it's sad to see you go."& 
     &//achar(10)// "Fear Is The Weakness, In Flames",&
+    
     "If this is my crown, I am its slave." & 
     &//achar(10)// "Death Can Take Me, Lorna Shore",&
+
+    "Wenn Fliegen hinter Fliegen fliegen, fliegen Fliegen hinter Fliegen her."&
+    &//achar(10)// "~A German tongue twister",&
+
+    "Thanks to quantum mechanics, we seem to do have a free will, but I prefer the deterministic world"&
+    &//achar(10)// "~based on a personal discussion (but Einstein and Bohr had that discussion too)",&
+
+    "Pine trees are the best trees, that is a fact."&
+    &//achar(10)// "~(definitely not my subjective opinion)",&
+
+
     "We're dancing like flames flickering in the night"& 
     &//achar(10)// "Pain Remains I, Lorna Shore" ]
 
