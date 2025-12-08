d498d37a25485391195b555d7ac318fa923da526
Lila Zapp <lilajulika.zapp@gmail.com>
2025-12-08 16:30:59 +0100

    including prova in compile.sh

--- build/compile.sh
+++ build/compile.sh
@@ -3,6 +3,7 @@
 
 gfortran -c ../External/f90getopt.F90 
 gfortran -c ../src/header.f90 
+gfortran -c ../src/prova.f90 
 gfortran -c ../src/parser.f90 
 gfortran -c ../src/force_field_calc.f90 
 gfortran -c ../src/main.f90 
